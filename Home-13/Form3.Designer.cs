dc5f9ca1ea89fc50982b792b418f6f389e71aab0
Олег <130392331+oket23@users.noreply.github.com>
2025-01-10 16:38:41 +0200

    Task 2 complite

--- Home-13/Form3.Designer.cs
+++ Home-13/Form3.Designer.cs
@@ -29,12 +29,22 @@
         private void InitializeComponent()
         {
             backToMenuButton = new Button();
+            replaceBtn = new Button();
+            userSearchWordLb = new Label();
+            userReplaceWordLb = new Label();
+            replaceTb = new TextBox();
+            searchTb = new TextBox();
+            statsLb = new Label();
+            taskLb = new Label();
+            allTextLb = new Label();
+            allTextRtb = new RichTextBox();
+            statsRtb = new RichTextBox();
             SuspendLayout();
             // 
             // backToMenuButton
             // 
             backToMenuButton.FlatStyle = FlatStyle.Flat;
-            backToMenuButton.Location = new Point(12, 399);
+            backToMenuButton.Location = new Point(12, 360);
             backToMenuButton.Name = "backToMenuButton";
             backToMenuButton.Size = new Size(95, 39);
             backToMenuButton.TabIndex = 3;
@@ -42,11 +52,117 @@
             backToMenuButton.UseVisualStyleBackColor = true;
             backToMenuButton.Click += backToMenuButton_Click;
             // 
+            // replaceBtn
+            // 
+            replaceBtn.FlatStyle = FlatStyle.Flat;
+            replaceBtn.Location = new Point(505, 360);
+            replaceBtn.Name = "replaceBtn";
+            replaceBtn.Size = new Size(95, 39);
+            replaceBtn.TabIndex = 4;
+            replaceBtn.Text = "Replace word";
+            replaceBtn.UseVisualStyleBackColor = true;
+            replaceBtn.Click += replaceBtn_Click;
+            // 
+            // userSearchWordLb
+            // 
+            userSearchWordLb.AutoSize = true;
+            userSearchWordLb.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            userSearchWordLb.Location = new Point(12, 107);
+            userSearchWordLb.Name = "userSearchWordLb";
+            userSearchWordLb.Size = new Size(156, 21);
+            userSearchWordLb.TabIndex = 5;
+            userSearchWordLb.Text = "Enter word to search:";
+            // 
+            // userReplaceWordLb
+            // 
+            userReplaceWordLb.AutoSize = true;
+            userReplaceWordLb.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            userReplaceWordLb.Location = new Point(12, 148);
+            userReplaceWordLb.Name = "userReplaceWordLb";
+            userReplaceWordLb.Size = new Size(161, 21);
+            userReplaceWordLb.TabIndex = 5;
+            userReplaceWordLb.Text = "Enter word to replace:";
+            // 
+            // replaceTb
+            // 
+            replaceTb.Location = new Point(174, 148);
+            replaceTb.Name = "replaceTb";
+            replaceTb.Size = new Size(100, 23);
+            replaceTb.TabIndex = 6;
+            // 
+            // searchTb
+            // 
+            searchTb.Location = new Point(174, 109);
+            searchTb.Name = "searchTb";
+            searchTb.Size = new Size(100, 23);
+            searchTb.TabIndex = 6;
+            // 
+            // statsLb
+            // 
+            statsLb.AutoSize = true;
+            statsLb.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            statsLb.Location = new Point(12, 195);
+            statsLb.Name = "statsLb";
+            statsLb.Size = new Size(55, 25);
+            statsLb.TabIndex = 5;
+            statsLb.Text = "Stats:";
+            // 
+            // taskLb
+            // 
+            taskLb.AutoSize = true;
+            taskLb.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            taskLb.Location = new Point(253, 9);
+            taskLb.Name = "taskLb";
+            taskLb.Size = new Size(78, 32);
+            taskLb.TabIndex = 5;
+            taskLb.Text = "Taks 2";
+            // 
+            // allTextLb
+            // 
+            allTextLb.AutoSize = true;
+            allTextLb.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            allTextLb.Location = new Point(303, 72);
+            allTextLb.Name = "allTextLb";
+            allTextLb.Size = new Size(160, 32);
+            allTextLb.TabIndex = 5;
+            allTextLb.Text = "Text from file:";
+            // 
+            // allTextRtb
+            // 
+            allTextRtb.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            allTextRtb.Location = new Point(312, 107);
+            allTextRtb.Name = "allTextRtb";
+            allTextRtb.ReadOnly = true;
+            allTextRtb.Size = new Size(273, 218);
+            allTextRtb.TabIndex = 7;
+            allTextRtb.Text = "";
+            // 
+            // statsRtb
+            // 
+            statsRtb.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            statsRtb.Location = new Point(12, 223);
+            statsRtb.Name = "statsRtb";
+            statsRtb.ReadOnly = true;
+            statsRtb.Size = new Size(262, 102);
+            statsRtb.TabIndex = 7;
+            statsRtb.Text = "";
+            // 
             // task2Form
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(612, 411);
+            Controls.Add(statsRtb);
+            Controls.Add(allTextRtb);
+            Controls.Add(searchTb);
+            Controls.Add(replaceTb);
+            Controls.Add(statsLb);
+            Controls.Add(allTextLb);
+            Controls.Add(taskLb);
+            Controls.Add(userReplaceWordLb);
+            Controls.Add(userSearchWordLb);
+            Controls.Add(replaceBtn);
             Controls.Add(backToMenuButton);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             MaximizeBox = false;
@@ -55,11 +171,23 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Taks 2";
             FormClosed += task2Form_FormClosed;
+            Load += task2Form_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button backToMenuButton;
+        private Button replaceBtn;
+        private Label userSearchWordLb;
+        private Label userReplaceWordLb;
+        private TextBox replaceTb;
+        private TextBox searchTb;
+        private Label statsLb;
+        private Label taskLb;
+        private Label allTextLb;
+        private RichTextBox allTextRtb;
+        private RichTextBox statsRtb;
     }
 }
--- Home-13/Form3.cs
+++ Home-13/Form3.cs
@@ -15,7 +15,8 @@ namespace Home_13
     {
         private mainMenu menu;
 
-        public task2Form(mainMenu mainMenu)  
+        private string path;
+        public task2Form(mainMenu mainMenu)
         {
             menu = mainMenu;
             InitializeComponent();
@@ -31,5 +32,76 @@ namespace Home_13
             menu.Show();
             Hide();
         }
+
+        private void task2Form_Load(object sender, EventArgs e)
+        {
+            allTextRtb.Clear();
+
+            path = Path.Combine("Task_2", "task2Text.txt");
+            Directory.CreateDirectory("Task_2");
+
+            using (var fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Read))
+            {
+                using (var sr = new StreamReader(fs))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        allTextRtb.Text += sr.ReadLine() + Environment.NewLine;
+                    }
+
+                }
+            }
+
+        }
+
+        private void replaceBtn_Click(object sender, EventArgs e)
+        {
+            statsRtb.Clear();
+
+            int replaceCount = 0;
+            string searchWord = searchTb.Text;
+            string replaceWord = replaceTb.Text;
+            string punctuation = " ,!:?.;";
+            string result = "";
+
+            string[] text = allTextRtb.Text.Split(" ".ToCharArray(),StringSplitOptions.RemoveEmptyEntries);
+
+            for(int i =0; i < text.Length; i++)
+            {
+                string cleanWord = text[i].Trim(punctuation.ToCharArray());
+                if (cleanWord.Equals(searchWord, StringComparison.OrdinalIgnoreCase))
+                {
+                    text[i] = text[i].Replace(cleanWord, replaceWord);
+                    replaceCount++;
+                }
+                result += text[i] + " ";
+            }
+            statsRtb.Text = $"Word \"{searchWord}\" was replaced by {replaceWord} {replaceCount} times";
+
+
+            allTextRtb.Clear();
+
+            using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+            {
+                using (var sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine(result);
+                }
+            }
+            //
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                using (var sr = new StreamReader(fs))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        allTextRtb.Text += sr.ReadLine();
+                    }
+
+                }
+            }
+            //
+        }
+
     }
 }
